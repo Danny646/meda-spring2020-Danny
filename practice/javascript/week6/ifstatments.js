d2a0768e5289a36c9003585f7f46977bd7f21626
student <student@PATC07.local>
2020-02-27 14:00:37 -0800

    revised ifstatments.js

---
+++ practice/javascript/week6/ifstatments.js
@@ -0,0 +1,54 @@
+//If-else Statements
+//If (condition) 
+if (true) {
+    console.log( "this code runs if this statement was true");
+}
+    console.log("End of script")
+
+    var age = 14;
+    if (age >=21){
+        console.log ("congrats, go get drunk");
+    }
+
+    var password = "abcdef";
+    if (password=="abcdef") {
+        console.log("welcome to your profile!");
+    }
+
+    var issleepy = true;
+    if (issleepy){
+    }
+        console.log ("Then go home and go to sleep");
+    
+        var iscranky= false;
+    if (iscranky) {
+    }
+        console.log("then go home and sleep");
+
+        var issmart = true;
+        if (issmart){
+        }
+            console.log ("Then you should pass all your classes with a great GPA");
+
+            var boughtacar = true;
+            if (boughtacar){
+              console.log("my car is great, I feel good with this purcahse, it's relaible and it looks fancy");  
+            }
+
+            var eatdonuts = true;
+            if (eatdonuts){
+            
+                console.log(" I ate donuts for breakfast, for I need to eat my sugar,  and was in a rush.")
+            }
+
+            var  buylatte = true;
+            if(buylatte){
+             console.log("I drank a Latte for it was a nice energy boost, in which I need despratley");   
+            }
+
+
+            var drankexpresso = true;
+           if (drankexpresso){ 
+               
+            console.log ("I drank an expresso in the AM,for we were going over complicated material");
+           }
