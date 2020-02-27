ae816ca46bc6ec014aae19ffdef93420bce80f76
student <student@PATC07.local>
2020-02-27 10:02:00 -0800

    creating a new javascript file, functionspractice.js

---
+++ practice/javascript/week6/functionspractice.js
@@ -0,0 +1,34 @@
+//Concatenation example.
+"a word" + "gabe";
+console.log("hello"+true);
+console.log(100+"a number")
+console.log(100+100+100+"hello";) 
+console.log(100+100+100+"hello");
+
+function sentencebuilder(person,place,action,item){
+    var firstpart="went to the";
+    var secondpart= ",and did";
+    var thirdpart= "but there was a problem,";
+    var fourthpart= "did not work.";
+    
+    var completesentence= person+firstpart+place+actionthirdpart+item+fourthpart;
+    console.log(completesentence);
+}
+ 
+sentencebuilder("Danny,went to "Mount Tam", and "fell on the famous bench" "Asurga01" but the stinky Octapus woke him up);
+
+
+function getnumber(){ 
+var thespecialnumber =42;
+return thespecialnumber;
+console.log(getnumber) );
+var newnumber= getnumber()+100; 
+
+function bestnumber (){ 
+var thisspecialnumber =17;
+return thisspecialnumber;
+console.log ( bestnumber));
+console.log( bestnumber)+100;
+console.log(newnumber);
+var results = bestnumber(/50);
+console.log(results);  
