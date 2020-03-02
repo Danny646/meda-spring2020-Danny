dbd1ac54f0482be1c4b7951e7fd2a764b87e6791
student <student@PATC07.local>
2020-03-02 13:14:28 -0800

    adding arrays.js

---
+++ practice/javascript/week7/arrays.js
@@ -0,0 +1,21 @@
+//var myvar = "one item";
+//var firstday = "monday";
+//var secondday = "tuesday";
+//Array syntax, a collection of data that is surrounded by square brackets. 
+
+//Requesting a specific value inside of the array: use square brackets after calling the variable with the array.
+//Put the index # inside of the square brackets. 
+var daysofweek= ["monday", "tuesday", "wednesday", "thursday", "friday",  "saturday", "sunday"];
+console.log(daysofweek[6]);
+daysofweek[6]="funday";
+console.log(daysofweek[6])
+daysofweek[12]="superday";
+console.log("superday");
+daysofweek[11]="multiplyday";
+console.log("multiplyday");
+//You can mix datatypes in an Array 100, true "hello"];
+
+
+var peoplesnames=["pete","tom", "renee", "owen", "luke", "ray", "john"]; 
+console.log(peoplesnames[6]);
+peoplesnames[6]="terry";
