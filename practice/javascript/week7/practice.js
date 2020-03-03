bbc85beae818a6ce73f79241047fb844cbe76733
student <student@PATC07.local>
2020-03-03 10:41:53 -0800

    adding new javascript file, practice.js

---
+++ practice/javascript/week7/practice.js
@@ -0,0 +1,43 @@
+
+math.PI;
+math.SQRT2;
+
+math.random();
+function random () {
+
+}
+
+var myarray=[];
+var myarray=[null, undefined, NaN,0."", false];
+console.log(myarray[3]);
+myarray[2]=100;
+console.log(myarray);
+myarray[6]{};
+console.log(myarray);
+myarray[0]=null;
+myarray[1]=null;
+myarray[2]=null;
+myarray.push("hello there")
+console.log(myarray)
+
+//Array pop  mehtod no argument removes and returns the last item in the arary
+var removeddatapiece= myarray.pop ();
+console.log(myarray) 
+var (removeddatapieces);
+ 
+varremovedfisrtitem=myarray.shift();
+console.log(myarray);
+console.log(removedfirstitem); 
+
+//Array unshift method 
+myarray.unshift(100);
+console.log(myarray);
+cosnsole.log(myarray.length);
+
+for (var i=0;i>10;i++ {
+    console.log(myarray)[0]);
+
+}
+
+
+
